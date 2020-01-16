13a47b38b886d77966d11830a48a310449f11e46
DK\mmd <mmd@sitecore.net>
2020-01-16 11:22:00 +0800

    initial commit

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NFC, Ndef } from '@ionic-native/nfc';
 
 @Component({
   selector: 'page-contact',
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ToastController } from 'ionic-angular';
+import { NFC, Ndef } from '@ionic-native/nfc';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private toastCtrl: ToastController,
+    private nfc: NFC,
+    private ndef: Ndef) {
 
   }
+  check(){
+    this.presentToast(this.nfc.enabled());
+  }
 
+  startRead(){
+    this.nfc.addNdefListener(() => {
+      console.log('successfully attached ndef listener');
+    }, (err) => {
+      console.log('error attaching ndef listener', err);
+    }).subscribe((event) => {
+      console.log('received ndef message. the tag contains: ', event.tag);
+      console.log('decoded tag id', this.nfc.bytesToHexString(event.tag.id));
+    
+      let message = this.ndef.textRecord('Hello world');
+      this.nfc.share([message]).then(onSuccess => {
+        this.presentToast(onSuccess.textRecord);
+      }).catch(onError => {
+        this.presentToast(onError);
+      });
+    });
+  }
+
+  presentToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000,
+      position: 'top'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
+  }
 }
